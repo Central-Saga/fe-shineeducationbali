127ee5a084b8111c81f836744bbcb93bcc177b8c
Borbobb <madeadisujaya23@gmail.com>
2025-09-25 23:35:16 +0800

    refactor: Update Font Usage in Global Styles and Layout
    - Replaced the font families in globals.css from Geist to Inter and JetBrains Mono for improved typography.
    - Updated layout.tsx to reflect the new font variables, ensuring consistent styling across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
   subsets: ["latin"],
 });
 
@@ -24,8 +24,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body className={`${geistSans.className}`}>{children}</body>
+    <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
